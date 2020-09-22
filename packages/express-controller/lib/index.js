121afb2bbadf0aa9f888f6428fcfd48b2ee3334e
nicholas j panella <nickjpanella@gmail.com>
2020-09-22 15:17:35 -0400

    ci: add src

---
+++ packages/express-controller/lib/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Hello = void 0;
+exports.Hello = 'hello!';
+//# sourceMappingURL=index.js.map
---
+++ typedoc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mode: 'modules',
+  out: 'docs/src',
+  exclude: ['**/node_modules/**', '**/*.spec.ts'],
+  name: 'Espress.JS',
+  excludePrivate: true,
+  readme: 'README.md'
+}
